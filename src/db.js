c7831f24670bb7f585890e40d14fdda72cf5d747
LeninCar <lenincarabali@gmail.com>
2025-12-01 12:56:51 -0500

    Añadir SSL a conexión Postgres en RDS

--- src/db.js
+++ src/db.js
@@ -7,10 +7,13 @@ const { Pool } = pg;
 
 export const pool = new Pool({
   host: process.env.PG_HOST,
-  port: process.env.PG_PORT,
+  port: process.env.PG_PORT || 5432,
   user: process.env.PG_USER,
   password: process.env.PG_PASSWORD,
-  database: process.env.PG_DATABASE
+  database: process.env.PG_DATABASE,
+  ssl: {
+    rejectUnauthorized: false, // 🔹 importante para RDS (equivale a sslmode=require)
+  },
 });
 
 pool.connect()
